6c56225bce45d86d818a92a677d2804726cf7668
Tommy R <drumming.dog101@gmail.com>
2025-08-03 19:47:14 -0400

    Switched off dev mode

--- gen-authenticate.js
+++ gen-authenticate.js
@@ -1,6 +1,4 @@
 function authenticate (value) {
-    return true;
-
     let proctorPassword;
     try {
         proctorPassword = JSON.parse(localStorage.newPW); //Check if a alternate proctor password is set
