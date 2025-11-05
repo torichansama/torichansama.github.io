f08a14bdb5060e4a1b0b307a11a49052f63f8f08
Tommy Radabaugh <drumming.dog101@gmail.com>
2025-11-05 02:32:53 -0500

    Disabled max scoring

--- gen-config.js
+++ gen-config.js
@@ -14,4 +14,4 @@ const defaultSettings = {
 const COMP_TESTING = false; //Use for correct screen sizing on non-iOS devices
 const DEBUG_VIEW = true; //Display debug stack on side of screen
 const SCORE_DEBUG = false; //Set this to view the scoring process for debugging purposes
-const FIND_MAX_SCORE = true; //Set this to true to find the maximum score for the selected figure
+const FIND_MAX_SCORE = false; //Set this to true to find the maximum score for the selected figure
