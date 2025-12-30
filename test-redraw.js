e4293e31b564655adda28625ec73d112b1101cc8
Tommy Radabaugh <drumming.dog101@gmail.com>
2025-12-29 21:22:23 -0500

    Starting to re-add snapshotting, testing removing bounding box detection

--- test-redraw.js
+++ test-redraw.js
@@ -40,9 +40,46 @@ function gridCtxRedraw() {
     if (DEBUG_VIEW) updateDebugView();
 }
 
+function grabSnapshot() {
+    strokeStartIndex = strokes.length;
+
+    let drawToCopyScale = COPY_W/W;
+    copyCtx.setTransform(drawToCopyScale*zoom, 0, 0, drawToCopyScale*zoom, offsetX*drawToCopyScale, offsetY*drawToCopyScale);
+
+    //Drawing strokes using one continuous line
+    for(let i = 0; i < strokes.length; i++) {
+        let stroke = strokes[i];
+        copyCtx.globalCompositeOperation = stroke.strokeColor == DRAW_COLOR ? "source-over" : "destination-out";
+
+        //Render single length strokes as circles since iOS doesn't render lines that end at the same point they start
+        if (Math.round(stroke.x[0]) == Math.round(stroke.x[stroke.x.length-1]) && Math.round(stroke.y[0]) == Math.round(stroke.y[stroke.y.length-1])) { 
+            copyCtx.setTransform(scale, 0, 0, scale, 0, 0);
+
+            copyCtx.fillStyle = stroke.strokeColor;
+            circle(stroke.x[0]*zoom+offsetX, stroke.y[0]*zoom+offsetY, stroke.brushSize*zoom, true, copyCtx);
+            copyCtx.setTransform(scale*zoom, 0, 0, scale*zoom, offsetX*scale, offsetY*scale);   
+            continue;
+        }
+
+        copyCtx.strokeStyle = stroke.strokeColor;
+        copyCtx.lineWidth = stroke.brushSize*2;
+
+        copyCtx.beginPath();
+        copyCtx.moveTo(stroke.x[0], stroke.y[0])
+        for (let i = 1; i < stroke.x.length; i++) {
+            copyCtx.lineTo(stroke.x[i], stroke.y[i]);
+        }
+        copyCtx.stroke();
+    }
+}
+
 //Drawing the content of the draw canvas-----------------------------------------------------------
 function drawCtxRedraw() {
     drawCtx.clearRect(0, 0, W, H);
+
+    // if (strokeStartIndex > 0) {;
+    //     drawCtx.drawImage(copyCanvas, offsetX-(W/2)*zoom, offsetY-(H/2)*zoom, W*zoom, H*zoom);
+    // }
 
     drawCtx.setTransform(scale*zoom, 0, 0, scale*zoom, offsetX*scale, offsetY*scale);
     let screenMinX = -offsetX/zoom;
@@ -53,13 +90,13 @@ function drawCtxRedraw() {
     let numStrokesRendered = 0;
 
     //Drawing strokes using one continuous line
-    for(let i = 0; i < strokes.length; i++) {
+    for(let i = strokeStartIndex; i < strokes.length; i++) {
         let stroke = strokes[i];
         drawCtx.globalCompositeOperation = stroke.strokeColor == DRAW_COLOR ? "source-over" : "destination-out";
 
-        if (!(stroke.minX < screenMaxX && stroke.maxX > screenMinX && stroke.minY < screenMaxY && stroke.maxY > screenMinY)) {
-            continue;
-        }
+        // if (!(stroke.minX < screenMaxX && stroke.maxX > screenMinX && stroke.minY < screenMaxY && stroke.maxY > screenMinY)) {
+        //     continue;
+        // }
         numStrokesRendered++;
 
         //Render single length strokes as circles since iOS doesn't render lines that end at the same point they start
