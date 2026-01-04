9c262a13294c8277829113732c9354583d1e85c9
Tommy R <drumming.dog101@gmail.com>
2026-01-03 19:23:23 -0500

    Quick test of 2x2 snapshot tiling

--- test-redraw.js
+++ test-redraw.js
@@ -44,8 +44,8 @@ function grabSnapshot() {
     strokeStartIndex = strokes.length;
 
     copyCtx.clearRect(0, 0, COPY_W, COPY_H);
-    let drawToCopyScale = COPY_W/W;
-    copyCtx.setTransform(drawToCopyScale, 0, 0, drawToCopyScale, COPY_W/2, COPY_H/2);
+    let drawToCopyScale = COPY_W/W*2;
+    copyCtx.setTransform(drawToCopyScale, 0, 0, drawToCopyScale, COPY_W, COPY_H);
 
     copyCtx.strokeStyle = DRAW_COLOR;
     copyCtx.fillStyle = DRAW_COLOR;
@@ -76,7 +76,7 @@ function drawCtxRedraw() {
     if (strokeStartIndex > 0 && zoom < SNAPSHOT_ZOOM_THRESH) {;
         drawCtx.globalCompositeOperation = "source-over";
         try {
-            drawCtx.drawImage(copyCanvas, offsetX-(W/2)*zoom, offsetY-(H/2)*zoom, W*zoom, H*zoom);
+            drawCtx.drawImage(copyCanvas, offsetX-(W/2)*zoom, offsetY-(H/2)*zoom, W*zoom/2, H*zoom/2);
         } catch (error) {
             drawCtx.fillStyle = "black";
             drawCtx.fillText(error, 5, 45);
